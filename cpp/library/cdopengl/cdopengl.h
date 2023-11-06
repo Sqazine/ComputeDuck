2f1672e5285cb0537b4451845123e7a084d9c5a9
Sqazine <Sqazine@163.com>
2023-11-06 17:41:48 +0800

    add simple opengl wrapper and example

---
+++ cpp/library/cdopengl/cdopengl.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <cstdint>
+#include "../../Config.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+	COMPUTE_DUCK_API void RegisterBuiltins();
+
+#ifdef __cplusplus
+}
+#endif
