2d59bc8075e7e5a7413bb7ff814154ce3fddc633
Sqazine <Sqazine@163.com>
2023-11-15 10:24:32 +0800

    import python impl

--- Config.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-#include <vector>
-#ifdef COMPUTE_DUCK_BUILD_DLL
-#define COMPUTE_DUCK_API __declspec(dllexport)
-#else
-#define COMPUTE_DUCK_API __declspec(dllimport)
-#endif
