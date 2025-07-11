4dc2642630a1da3d64341eec597686b5029557db
Sqazine <Sqazine@163.com>
2025-07-11 18:33:22 +0800

    add Sample comment

--- book-sample-code/Chapter2/Token.h
+++ book-sample-code/Chapter2/Token.h
@@ -7,29 +7,29 @@
 enum class TokenType
 {
     NUMBER = 0,
-    SEMICOLON,	   // ;
-    LPAREN,		   // (
-    RPAREN,		   // )
-    PLUS,		   // +
-    MINUS,		   // -
-    ASTERISK,	   // *
-    SLASH,		   // /
-    LESS,		   // <
-    GREATER,	   // >
-    VBAR,		   // |
-    CARET,		   // ^
-    AMPERSAND,	   // &
-    TILDE,		   // ~
+    SEMICOLON,     // ;
+    LPAREN,        // (
+    RPAREN,        // )
+    PLUS,          // +
+    MINUS,         // -
+    ASTERISK,      // *
+    SLASH,         // /
+    LESS,          // <
+    GREATER,       // >
+    VBAR,          // |
+    CARET,         // ^
+    AMPERSAND,     // &
+    TILDE,         // ~
     EQUAL_EQUAL,   // ==
-    LESS_EQUAL,	   // <=
+    LESS_EQUAL,    // <=
     GREATER_EQUAL, // >=
-    BANG_EQUAL,	   // !=
-    TRUE,		   // true
-    FALSE,		   // false
-    NIL,		   // nil
-    AND,		   // and
-    OR,			   // or
-    NOT,		   // not
+    BANG_EQUAL,    // !=
+    TRUE,          // true
+    FALSE,         // false
+    NIL,           // nil
+    AND,           // and
+    OR,            // or
+    NOT,           // not
     END
 };
 
@@ -37,11 +37,11 @@ struct Token
 {
     Token(TokenType type, std::string_view literal, uint32_t line, uint32_t column, std::string_view filePath) : type(type), literal(literal), line(line), column(column), filePath(filePath) {}
 
-    std::string filePath;
     TokenType type;
-    std::string literal;
-    uint32_t line;
-    uint32_t column;
+    std::string filePath; // 当前Token所在的文件路径
+    std::string literal;  // 当前Token字面量
+    uint32_t line;        // 当前Token在文件中所在的行
+    uint32_t column;      // 当前Token在文件中所在的列
 };
 
 inline std::ostream &operator<<(std::ostream &stream, const Token &token)
