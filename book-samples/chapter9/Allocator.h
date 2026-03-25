c853ecd5c6f3954e942617a12fd5b42933f002a5
Sqazine <Sqazine@163.com>
2026-03-25 14:25:17 +0800

    add chapter9

---
+++ book-samples/chapter9/Allocator.h
@@ -0,0 +1,58 @@
+#pragma once
+#include "Utils.h"
+#include "Value.h"
+
+template <typename T>
+concept IsChildOfObject = !std::is_same_v<T, void> &&
+                          !std::is_abstract_v<T> &&
+                          std::is_base_of_v<Object, T>;
+
+class COMPUTEDUCK_API Allocator
+{
+public:
+    static Allocator *GetInstance();
+
+    void Init();
+    void Destroy();
+    void ResetStatus();
+
+    void Push(const Value &value);
+    Value Pop();
+
+    Value *GetGlobalVariableSlot(size_t index);
+
+    Value *GetStackTop() { return m_StackTop; }
+
+    void StackTopJump(size_t slotCount);
+
+    template <IsChildOfObject T, typename... Args>
+    T *AllocateObject(Args &&...params)
+    {
+        T *object = new T(std::forward<Args>(params)...);
+        return object;
+    }
+
+    
+    Value *GetLocalVariableSlot(int16_t index);
+    
+private:
+    Allocator() = default;
+    ~Allocator() = default;
+
+    Value *m_StackTop{nullptr};
+    Value m_ValueStack[STACK_COUNT];
+
+    Value m_GlobalVariables[STACK_COUNT];
+};
+
+#define GET_GLOBAL_VARIABLE_SLOT(x) (Allocator::GetInstance()->GetGlobalVariableSlot(x))
+
+#define PUSH(x) (Allocator::GetInstance()->Push(x))
+#define POP() (Allocator::GetInstance()->Pop())
+
+#define GET_STACK_TOP() (Allocator::GetInstance()->GetStackTop())
+#define STACK_TOP_JUMP(x) (Allocator::GetInstance()->StackTopJump(x))
+
+
+#define GET_LOCAL_VARIABLE_SLOT(idx) (Allocator::GetInstance()->GetLocalVariableSlot(idx))
+
