a068bea02de79b051e3e67f6232676ff3b9aa6bd
Sqazine <Sqazine@163.com>
2026-05-19 13:37:18 +0800

    add ch 18

---
+++ book-samples/chapter18/Config.cpp
@@ -0,0 +1,27 @@
+#include "Config.h"
+#include <filesystem>
+
+Config *Config::GetInstance()
+{
+    static Config instance;
+    return &instance;
+}
+
+void Config::SetExecuteFileDirectory(std::string_view path)
+{
+    m_CurExecuteFileDirectory = path;
+}
+
+const std::string &Config::GetExecuteFileDirectory() const
+{
+    return m_CurExecuteFileDirectory;
+}
+
+std::string Config::ToFullPath(std::string_view filePath)
+{
+    std::filesystem::path filesysPath = filePath;
+    std::string fullPath = filesysPath.string();
+    if (!filesysPath.is_absolute())
+        fullPath = m_CurExecuteFileDirectory + fullPath;
+    return fullPath;
+}
