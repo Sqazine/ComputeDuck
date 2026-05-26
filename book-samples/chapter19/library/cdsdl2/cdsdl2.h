13a2b91ee4ce40ee599e3a0aff3d16b60961ade7
Sqazine <Sqazine@163.com>
2026-05-26 10:11:00 +0800

    add ch19

---
+++ book-samples/chapter19/library/cdsdl2/cdsdl2.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#ifdef _WIN32
+#ifdef COMPUTEDUCK_BUILD_DLL
+#define COMPUTEDUCK_API __declspec(dllexport)
+#else
+#define COMPUTEDUCK_API __declspec(dllimport)
+#endif
+#else
+#define COMPUTEDUCK_API
+#endif
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    COMPUTEDUCK_API void RegisterBuiltins();
+
+#ifdef __cplusplus
+}
+#endif
